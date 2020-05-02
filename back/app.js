cbed51751d51d86e861f14aee0d55810feaa413e
isa <isabel.nieto@comafi.com.ar>
2020-05-02 00:21:42 -0300

    agregado middleware para leer formularios

--- back/app.js
+++ back/app.js
@@ -1,14 +1,18 @@
-var express = require('express')
-var app = express();
-var guest_route = require("./routes/guest");
-var admin_route = require("./routes/admin");
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express();
+const guest_route = require("./routes/guest");
+const admin_route = require("./routes/admin");
+
+const middlewares = [
+    // express.static(path.join(__dirname, 'public')),
+    bodyParser.urlencoded({ extended: true }),
+    bodyParser.json()
+];
+app.use(middlewares);
 
 app.use('/', guest_route);
 app.use('/admin', admin_route);
-
-app.get('/admin', function (req, res) {
-    res.send('hello world')
-})
 
 app.listen(3000, (item) => {
     console.log(item)
--- back/routes/guest.js
+++ back/routes/guest.js
@@ -1,9 +1,14 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router(['strict']);
 
-// define the home page route
-router.get('/', (req, res) => {
-    res.send('Birds home page');
+//define the data for form ingresed
+// router.get('/', (req, res) => {
+//     res.redirect("/register_payment")
+// });
+
+router.post('/register_payment', (req, res) => {
+    console.log(JSON.stringify(req.body));
+    res.status(202).json({ error: null, data: "Ok"})
 });
 
 module.exports = router;
